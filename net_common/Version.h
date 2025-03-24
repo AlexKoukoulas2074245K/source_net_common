c8c56efec0fef62ddca585b4af2f60dcc86d3e09
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-03-24 16:52:50 +0000

    Dummy

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define LIB_VERSION "1.1.3"        // PATCH Updated automatically
-#define LIB_COMMIT_HASH "933bacb"  // Updated automatically every commit
+#define LIB_VERSION "1.1.4"        // PATCH Updated automatically
+#define LIB_COMMIT_HASH "5c2bb27"  // Updated automatically every commit
 
 #endif // VERSION_H
