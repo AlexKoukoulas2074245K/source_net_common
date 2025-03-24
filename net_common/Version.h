933bacb8e2f506cba362e135c2c484b1374c9646
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-03-24 16:45:16 +0000

    Dummy commit testing versioning

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define LIB_VERSION "1.1.117"        // PATCH Updated automatically
-#define LIB_COMMIT_HASH "6762b16"  // Updated automatically every commit
+#define LIB_VERSION "1.1.2"        // PATCH Updated automatically
+#define LIB_COMMIT_HASH "1bb9b12"  // Updated automatically every commit
 
 #endif // VERSION_H
