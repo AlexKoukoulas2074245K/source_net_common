e1d735ce56f7906a797e27b86d3767ee7f3ac0db
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-03-24 16:24:18 +0000

    Added source_net_common revision file

---
+++ net_common/Version.h
@@ -0,0 +1,7 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define LIB_VERSION "1.1.1"        // PATCH Updated automatically
+#define LIB_COMMIT_HASH "abcd123"  // Updated automatically every commit
+
+#endif // VERSION_H
