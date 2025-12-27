e7e6c69c5ee8e49ebb5cc1cbd5aeaba50dc9883c
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-12-27 20:44:24 +0000

    Added player ID assignment message

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define NET_COMMON_VERSION "1.1.23"        // PATCH Updated automatically
-#define NET_COMMON_COMMIT_HASH "6affafd"  // Updated automatically every commit
+#define NET_COMMON_VERSION "1.1.24"        // PATCH Updated automatically
+#define NET_COMMON_COMMIT_HASH "b7e18aa"  // Updated automatically every commit
 
 #endif // VERSION_H
