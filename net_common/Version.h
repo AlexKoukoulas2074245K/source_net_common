035337e76340df8632ff762b19582886afd0dbdf
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-12-28 14:55:17 +0000

    Added velocity to snapshot

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define NET_COMMON_VERSION "1.1.25"        // PATCH Updated automatically
-#define NET_COMMON_COMMIT_HASH "e7e6c69"  // Updated automatically every commit
+#define NET_COMMON_VERSION "1.1.26"        // PATCH Updated automatically
+#define NET_COMMON_COMMIT_HASH "9e72775"  // Updated automatically every commit
 
 #endif // VERSION_H
