5dfc6644f03b13e5b0344a9772f11d37850e0438
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2026-01-27 15:10:37 +0000

    Reinstated Navmap wrapper class in net_common

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define NET_COMMON_VERSION "1.1.38"        // PATCH Updated automatically
-#define NET_COMMON_COMMIT_HASH "855174b"  // Updated automatically every commit
+#define NET_COMMON_VERSION "1.1.39"        // PATCH Updated automatically
+#define NET_COMMON_COMMIT_HASH "035fcae"  // Updated automatically every commit
 
 #endif // VERSION_H
