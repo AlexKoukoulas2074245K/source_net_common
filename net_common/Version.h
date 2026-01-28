e3f3bb2695bba29ed0fa743ac3d1dd6c650e4ee8
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2026-01-28 17:17:55 +0000

    Reinstated net_assets

--- net_common/Version.h
+++ net_common/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define NET_COMMON_VERSION "1.1.40"        // PATCH Updated automatically
-#define NET_COMMON_COMMIT_HASH "5dfc664"  // Updated automatically every commit
+#define NET_COMMON_VERSION "1.1.41"        // PATCH Updated automatically
+#define NET_COMMON_COMMIT_HASH "c6f37d9"  // Updated automatically every commit
 
 #endif // VERSION_H
